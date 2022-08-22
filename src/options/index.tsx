31af9461c11db5d6353af791ffd4caad895888e4
Siwakorn <cardeer.dev@gmail.com>
2022-08-22 21:11:42 +0700

    chore: change jsx option in tsconfig

---
+++ src/options/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/global.scss";
+
+ReactDOM.createRoot(document.querySelector("#root")!).render(
+  <React.StrictMode>
+    <h1>Hello, world this is options page</h1>
+  </React.StrictMode>
+);
